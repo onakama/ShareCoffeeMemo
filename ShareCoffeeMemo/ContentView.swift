920d5639d13a382d2ae3eddb6148b03376fd4052
sumi <ninzin60en.dev@gmail.com>
2021-10-26 00:31:32 +0900

    Initial Commit

---
+++ ShareCoffeeMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareCoffeeMemo
+//
+//  Created by nekuro on 2021/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShareCoffeeMemo/ShareCoffeeMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareCoffeeMemoApp.swift
+//  ShareCoffeeMemo
+//
+//  Created by nekuro on 2021/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ShareCoffeeMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
