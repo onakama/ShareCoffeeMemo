9ac985321f812d942d13eed6d9691fde3eebda94
taisei <ninzin60en.dev@gmail.com>
2021-12-16 23:15:44 +0900

    commit

--- ShareCoffeeMemo/Memo/Model/MomoModel.swift
+++ ShareCoffeeMemo/Memo/Model/MomoModel.swift
@@ -59,21 +59,6 @@ class PubMemoModel: Codable, MemoProtocol, Identifiable {
     var roast: Int = 0
     var review: String = ""
     var date: Date = Date()
-    func PubMemoModel(memo: MemoProtocol) {
-        self.id = memo.id
-        self.name = memo.name
-        self.saler = memo.saler
-        self.taste = memo.taste
-        self.body = memo.body
-        self.roast = memo.roast
-        self.review = memo.review
-        self.date = memo.date
-    }
 }
 
-let jsonStr = """
-{
-"statusCode": 200, "body": "[{\"id\": 1, \"name\": \"sample1\", \"saler\": \"shop3\", \"taste\": 1, \"body\": 1, \"roast\": 1, \"review\": \"うまい", \"date\": \"2021-10-25T10:58:00\"}, {\"id\": 2, \"name\": \"sample2\", \"saler\": \"shop2\", \"taste\": 0, \"body\": 0, \"roast\": 0, \"review\": \"うまい", \"date\": \"2021-10-25T10:58:00\"}, {\"id\": 3, \"name\": \"sample1\", \"saler\": \"shop1\", \"taste\": 0, \"body\": 0, \"roast\": 0, \"review\": \"ゲロ", \"date\": \"2021-10-25T10:58:00\"}, {\"id\": 4, \"name\": \"sample2\", \"saler\": \"shop2\", \"taste\": 2, \"body\": 2, \"roast\": 2, \"review\": \"神", \"date\": \"2021-10-20T15:58:00\"}]"
-}
-"""
 
